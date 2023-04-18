b3e8eeece21df83a82187635cab75b085b98f050
Felipe Reis <111073491+felipereis068@users.noreply.github.com>
2023-04-18 14:26:28 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './components/Login';
+
+// Faz a função App retorna Login.jsx
+export default function App() {
+  return(
+  <div>
+    <Login />
+  </div>
+  );
+}
---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Logins() {
+    return  <Container>
+        <img src="" alt="" />
+        <button>Connect Spotify</button>
+    </Container>;
+}
+
+const Container = styled.div``;
