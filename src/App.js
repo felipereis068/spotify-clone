0d5393ba031dc05ba18c9d1aeb9446f8bef76bdd
Felipe Reis <111073491+felipereis068@users.noreply.github.com>
2023-04-19 16:07:47 -0500

    StateUser and API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Login from './components/Login';
+import { reducerCases } from './utils/Constants';
+import { useStateProvider} from './utils/StateProvider'
+
 
 // Faz a função App retorna Login.jsx
 export default function App() {
+  const [{token}, dispatch]=useStateProvider()
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      console.log(token);
+      dispatch({action:reducerCases.SET_TOKEN, token});
+    }
+  }, [token, dispatch])
   return(
   <div>
     <Login />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,10 +2,53 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Logins() {
-    return  <Container>
-        <img src="" alt="" />
-        <button>Connect Spotify</button>
-    </Container>;
+    const handleClick = () => {
+        const clientId = "7c9bb9498b144d9c83e32edf187eddcb";
+        const redirectUrl = "http://localhost:3000/";
+        const apiUrl ="http://accounts.spotify.com/authorize";
+        const scope = [
+            'user-read-email',
+            'user-read-private',
+            'user-modify-playback-state',
+            'user-read-playback-state',
+            'user-read-currently-playing',
+            'user-read-recently-played',
+            'user-read-playback-position',
+            'user-top-read',
+        ];
+        window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(
+            " "
+        )}&response_type=token&show_daialog=true`;
+    };
+    return ( <Container>
+        <img 
+            src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" 
+            alt="spotify" 
+        />
+        <button onClick={handleClick}>Connect Spotify</button>
+    </Container>
+    );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+    background-color #1db954;
+    gab 2rem;
+    img {
+    height: 20vh;
+    }
+    button {
+        padding: 1rem 5rem;
+        border-radius: 5rem;
+        border: none;
+        background-color: black;
+        color:#49f585;
+        font-siz 1.4rem;
+        cursor: pointer;
+    }
+`   
