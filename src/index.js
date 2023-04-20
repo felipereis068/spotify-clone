c9bedee24ac47c60c3e708198425e7b480cddd58
Felipe Reis <111073491+felipereis068@users.noreply.github.com>
2023-04-20 16:08:28 -0500

    sidebar,foter,body,playlists

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
-import './index.css';
-import App from './App';
-import { StateProvider } from './utils/StateProvider';
-import reducer, { initialState } from './utils/reducer';
-
+import "./index.css";
+import App from "./App.jsx";
+import { StateProvider } from "./utils/StateProvider";
+import reducer, { initialState } from "./utils/Reducer";
 
 ReactDOM.render(
   <React.StrictMode>
